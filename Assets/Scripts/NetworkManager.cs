adc1d65d42299f47310e7f63ded3153b8e9c8a70
dgeisert <memphis.gatech@gmail.com>
2017-02-23 17:06:31 -0800

    chat working

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class NetworkManager : MonoBehaviour {
+
+
+	protected Callback<GameOverlayActivated_t> Callback_GameOverlayActivated;
+	protected Callback<LobbyCreated_t> Callback_lobbyCreated;
+	protected Callback<LobbyMatchList_t> Callback_lobbyList;
+	protected Callback<LobbyEnter_t> Callback_lobbyEnter;
+	protected Callback<LobbyDataUpdate_t> Callback_lobbyInfo;
+	protected Callback<GameLobbyJoinRequested_t> Callback_joinLobby;
+	protected Callback<P2PSessionRequest_t> Callback_p2PSessionRequest;
+	private List<CSteamID> ExpectingClient = new List<CSteamID>();
+	private CSteamID lobbyID;
+	private bool initialized = false;
+
+	// Use this for initialization
+	void Start () {
+		if(SteamManager.Initialized && !initialized) {
+			SteamAPI.Init ();
+			Callback_GameOverlayActivated = Callback<GameOverlayActivated_t>.Create(OnGameOverlayActivated);
+			Callback_lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+			Callback_lobbyList = Callback<LobbyMatchList_t>.Create(OnLobbyList);
+			Callback_lobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEnter);
+			Callback_lobbyInfo = Callback<LobbyDataUpdate_t>.Create(OnLobbyInfo);
+			Callback_joinLobby = Callback<GameLobbyJoinRequested_t>.Create(OnJoinRequest);
+			Callback_p2PSessionRequest = Callback<P2PSessionRequest_t>.Create(OnP2PSessionRequest);
+		}
+	}
+
+	void Talk(){
+		uint voiceBytes;
+		uint voice2Bytes;
+		EVoiceResult voiceResult = SteamUser.GetAvailableVoice (out voiceBytes, out voice2Bytes, 0);
+		if (voiceBytes > 0) {
+			byte[] voice = new byte[voiceBytes];
+			byte[] voice2 = new byte[voiceBytes + 1];
+			SteamUser.GetVoice (true, voice, voiceBytes, out voiceBytes, false, voice2, 0, out voice2Bytes, 0);
+			voice.CopyTo (voice2, 1);
+			voice2 [0] = (byte)1;
+			SendBytes (voice2);
+		}
+	}
+
+	void SendString(string str){
+		byte[] bytes = new byte[str.Length * sizeof(char)];
+		System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+		SendBytes (bytes);
+	}
+
+	public void SendBytes(byte[] bytes){
+		foreach (CSteamID csid in ExpectingClient) {
+			SteamNetworking.SendP2PPacket(csid, bytes, (uint) bytes.Length, EP2PSend.k_EP2PSendReliable);
+		}
+	}
+
+	public void OnGameOverlayActivated(GameOverlayActivated_t overlay){
+
+	}
+
+	public void OnLobbyCreated(LobbyCreated_t lobbyCreated){
+		lobbyID = new CSteamID (lobbyCreated.m_ulSteamIDLobby);
+		SteamFriends.ActivateGameOverlayInviteDialog (lobbyID);
+		Debug.Log ("Lobby Created");
+	}
+
+	public void OnLobbyList(LobbyMatchList_t lobbyList){
+		Debug.Log ("Lobby List");
+	}
+
+	public void OnLobbyEnter(LobbyEnter_t lobbyEnter){
+		Debug.Log ("Lobby Entered");
+		lobbyID = new CSteamID (lobbyEnter.m_ulSteamIDLobby);
+	}
+
+	public void OnLobbyInfo(LobbyDataUpdate_t lobbyInfo){
+		Debug.Log ("Lobby Info");
+		SteamUser.StartVoiceRecording ();
+		ExpectingClient = new List<CSteamID> ();
+		int lobbyCount = SteamMatchmaking.GetNumLobbyMembers (lobbyID);
+		for (int i = 0; i < lobbyCount; i++) {
+			CSteamID csid = SteamFriends.GetFriendFromSourceByIndex (lobbyID, i);
+			if (!ExpectingClient.Contains (csid) && !(SteamUser.GetSteamID ().m_SteamID == csid.m_SteamID)) {
+				ExpectingClient.Add(SteamFriends.GetFriendFromSourceByIndex (lobbyID, i));
+			}
+		}
+		foreach (CSteamID csid in ExpectingClient) {
+			Debug.Log (SteamFriends.GetFriendPersonaName (csid));
+		}
+	}
+
+	public void OnJoinRequest(GameLobbyJoinRequested_t joinRequest){
+		Debug.Log ("Requested to join lobby");
+		JoinLobby (joinRequest.m_steamIDLobby);
+	}
+
+	void OnP2PSessionRequest(P2PSessionRequest_t request)
+	{
+		CSteamID clientId = request.m_steamIDRemote;
+		if (ExpectingClient.Contains(clientId))
+		{
+			SteamNetworking.AcceptP2PSessionWithUser(clientId);
+		} else {
+			Debug.LogWarning("Unexpected session request from " + clientId);
+		}
+	}
+
+	public void Update(){
+		if(Input.GetKeyDown(KeyCode.S)){
+			SetupServer ();
+		}
+		if(Input.GetKeyDown(KeyCode.I)){
+			SteamFriends.ActivateGameOverlayInviteDialog (lobbyID);
+		}
+		if(Input.GetKeyDown(KeyCode.C)){
+			SetupServer ();
+		}
+		Talk ();
+		ReadPackets ();
+	}
+	public void ReadPackets(){
+		uint size;
+
+		// repeat while there's a P2P message available
+		// will write its size to size variable
+		while (SteamNetworking.IsP2PPacketAvailable(out size))
+		{
+			
+			var buffer = new byte[size];
+			uint bytesRead;
+			CSteamID remoteId;
+
+			if (SteamNetworking.ReadP2PPacket(buffer, size, out bytesRead, out remoteId))
+			{
+				int dataType = (int)buffer [0];
+				byte[] dataIn = new byte[size - 1];
+				Array.Copy (buffer, 1, dataIn, 0, size - 1);
+				switch (dataType) {
+				case 1:
+					Debug.Log ("got audio");
+					byte[] bufferOut = new byte[22050];
+					uint bytesOut;
+					EVoiceResult voiceOut = SteamUser.DecompressVoice (dataIn, (uint)dataIn.Length, bufferOut, 22050, out bytesOut, 11025);
+					PlayerMachine.instance.chatAudio.clip = AudioClip.Create ("chat", 11025, 1, 11025, false, false);
+					float[] test = new float[11025];
+					for (int i = 0; i < test.Length; ++i) {
+						test[i] = (short)(bufferOut[i * 2] | bufferOut[i * 2 + 1] << 8) / 32768.0f;
+					}
+					PlayerMachine.instance.chatAudio.clip.SetData (test, 0);
+					PlayerMachine.instance.chatAudio.Play ();
+					break;
+				default:
+					// convert to string
+					char[] chars = new char[bytesRead / sizeof(char)];
+					System.Buffer.BlockCopy(dataIn, 0, chars, 0, dataIn.Length);
+
+					string message = new string(chars, 0, chars.Length);
+					Debug.Log("Received a message: " + message);
+					break;
+				}
+			}
+		}
+	}
+	public void SetupServer()
+	{
+		if (SteamManager.Initialized) {
+			CreateLobby (ELobbyType.k_ELobbyTypeFriendsOnly, 2);
+		}
+	}
+
+	SteamAPICall_t CreateLobby (ELobbyType eLobbyType, int maxMembers){
+		return SteamMatchmaking.CreateLobby (eLobbyType, maxMembers);
+	}
+
+	SteamAPICall_t JoinLobby (CSteamID lobby){
+		return SteamMatchmaking.JoinLobby (lobby);
+	}
+
+	bool InviteUserToLobby(CSteamID lobby, CSteamID invitee){
+		return false;
+	}
+}
--- Assets/Scripts/StateMachines/PlayerMachine.cs
+++ Assets/Scripts/StateMachines/PlayerMachine.cs
@@ -1,8 +1,6 @@
 ﻿using UnityEngine;
-using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
-using Steamworks;
 
 public class PlayerMachine : StateMachine {
 
@@ -12,15 +10,7 @@ public class PlayerMachine : StateMachine {
 	public StateMaster stateMaster;
 	private Dictionary<string, int> resources;
 	private Dictionary<string, GameObject> loadedResources;
-	protected Callback<GameOverlayActivated_t> Callback_GameOverlayActivated;
-	protected Callback<LobbyCreated_t> Callback_lobbyCreated;
-	protected Callback<LobbyMatchList_t> Callback_lobbyList;
-	protected Callback<LobbyEnter_t> Callback_lobbyEnter;
-	protected Callback<LobbyDataUpdate_t> Callback_lobbyInfo;
-	protected Callback<GameLobbyJoinRequested_t> Callback_joinLobby;
-	protected Callback<P2PSessionRequest_t> Callback_p2PSessionRequest;
-	private List<CSteamID> ExpectingClient = new List<CSteamID>();
-	private CSteamID lobbyID;
+	public AudioSource chatAudio;
 
 	public override void InstanceInitiate(StateMachine checkMachine){
 		stateMaster.Setup ();
@@ -51,127 +41,6 @@ public class PlayerMachine : StateMachine {
 			transform.rotation = AreaStartStateMachine.instance.transform.rotation;
 		}
 		LoadGos ();
-		if(SteamManager.Initialized) {
-			Callback_GameOverlayActivated = Callback<GameOverlayActivated_t>.Create(OnGameOverlayActivated);
-			Callback_lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
-			Callback_lobbyList = Callback<LobbyMatchList_t>.Create(OnLobbyList);
-			Callback_lobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEnter);
-			Callback_lobbyInfo = Callback<LobbyDataUpdate_t>.Create(OnLobbyInfo);
-			Callback_joinLobby = Callback<GameLobbyJoinRequested_t>.Create(OnJoinRequest);
-			Callback_p2PSessionRequest = Callback<P2PSessionRequest_t>.Create(OnP2PSessionRequest);
-			string name = SteamFriends.GetPersonaName();
-			Debug.Log(name);
-			if (name == "geisert" && isAtStartup) {
-				Debug.Log ("Starting Server");
-				SetupServer ();
-			}
-		}
-	}
-
-	void SendString(string str){
-		byte[] bytes = new byte[str.Length * sizeof(char)];
-		System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
-		SendBytes (bytes);
-	}
-
-	public void SendBytes(byte[] bytes){
-		foreach (CSteamID csid in ExpectingClient) {
-			SteamNetworking.SendP2PPacket(csid, bytes, (uint) bytes.Length, EP2PSend.k_EP2PSendReliable);
-		}
-	}
-
-	public void OnGameOverlayActivated(GameOverlayActivated_t overlay){
-
-	}
-
-	public void OnLobbyCreated(LobbyCreated_t lobbyCreated){
-		lobbyID = CSteamID (lobbyCreated.m_ulSteamIDLobby);
-		SteamFriends.ActivateGameOverlayInviteDialog (lobbyID);
-		Debug.Log ("Lobby Created");
-	}
-
-	public void OnLobbyList(LobbyMatchList_t lobbyList){
-		Debug.Log ("Lobby List");
-	}
-
-	public void OnLobbyEnter(LobbyEnter_t lobbyEnter){
-		lobbyID = CSteamID (lobbyEnter.m_ulSteamIDLobby);
-		int lobbyCount = SteamMatchmaking.GetNumLobbyMembers (lobbyID);
-		for (int i = 0; i < lobbyCount; i++) {
-			ExpectingClient.Add(SteamFriends.GetFriendFromSourceByIndex (lobbyID, i));
-		}
-		foreach (CSteamID csid in ExpectingClient) {
-			Debug.Log (SteamFriends.GetFriendPersonaName (csid));
-		}
-	}
-
-	public void OnLobbyInfo(LobbyDataUpdate_t lobbyInfo){
-		Debug.Log ("Lobby Info");
-	}
-
-	public void OnJoinRequest(GameLobbyJoinRequested_t joinRequest){
-		Debug.Log ("Requested to join lobby");
-		JoinLobby (joinRequest.m_steamIDLobby);
-	}
-
-	void OnP2PSessionRequest(P2PSessionRequest_t request)
-	{
-		CSteamID clientId = request.m_steamIDRemote;
-		if (ExpectingClient.Contains(clientId))
-		{
-			SteamNetworking.AcceptP2PSessionWithUser(clientId);
-		} else {
-			Debug.LogWarning("Unexpected session request from " + clientId);
-		}
-	}
-
-	public override void InstanceUpdate(StateMachine checkMachine){
-		if(Input.GetKeyDown(KeyCode.C)){
-			SetupLocalClient ();
-		}
-	}
-
-	NetworkClient myClient;
-	bool isAtStartup = true;
-	// Create a server and listen on a port
-	public void SetupServer()
-	{
-		if (SteamManager.Initialized) {
-			CreateLobby (ELobbyType.k_ELobbyTypeFriendsOnly, 2);
-		}
-	}
-
-	SteamAPICall_t CreateLobby (ELobbyType eLobbyType, int maxMembers){
-		return SteamMatchmaking.CreateLobby (eLobbyType, maxMembers);
-	}
-
-	SteamAPICall_t JoinLobby (CSteamID lobby){
-		return SteamMatchmaking.JoinLobby (lobby);
-	}
-
-	bool InviteUserToLobby(CSteamID lobby, CSteamID invitee){
-		return false;
-	}
-
-	// Create a client and connect to the server port
-	public void SetupClient()
-	{
-		myClient = new NetworkClient();
-		myClient.RegisterHandler(MsgType.Connect, OnConnected);
-		myClient.Connect("127.0.0.1", 4444);
-		isAtStartup = false;
-	}
-
-	// Create a local client and connect to the local server
-	public void SetupLocalClient()
-	{
-		myClient = ClientScene.ConnectLocalServer();
-		myClient.RegisterHandler(MsgType.Connect, OnConnected);     
-		isAtStartup = false;
-	}
-
-	public void OnConnected(NetworkMessage nmsg){
-		Debug.Log (nmsg.ToString());
 	}
 
 	public int GetResource(string resource){
